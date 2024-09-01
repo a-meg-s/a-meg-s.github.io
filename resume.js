792ccac252b3779ea2cf45bf0e5d624b8589f03e
Megan <codebymegan@gmail.com>
2024-09-01 16:48:36 +0200

    Update resume.js

--- resume.js
+++ resume.js
@@ -6,6 +6,7 @@ function typeWriter(text, n) {
     setTimeout(() => typeWriter(text, n + 1), 100);
   } else {
     element.innerHTML = text + '<span class="blinking-cursor">|</span>';
+    // Ensure the cursor keeps blinking after typing is finished
     setInterval(() => {
       const cursor = document.querySelector(".blinking-cursor");
       if (cursor) {
@@ -122,13 +123,15 @@ function hoverSkillBar() {
   });
 }
 
-function updateScrollIndicator() {
-  const indicatorDots = document.querySelectorAll(".indicator-dot");
-  indicatorDots.forEach((dot, index) => {
-    dot.classList.toggle("active", index === currentIndex);
-  });
-}
+window.addEventListener("click", () => {
+  const canvas = document.getElementById("profile-pic");
+  if (canvas && canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+    blinkEyes(ctx);
+  }
+});
 
+// Experience Carousel Logic
 let currentIndex = 0;
 
 const experienceInner = document.querySelector(".experience-inner");
@@ -138,13 +141,11 @@ const totalExperiences = experiences.length;
 document.getElementById("next-experience").addEventListener("click", () => {
   currentIndex = (currentIndex + 1) % totalExperiences;
   updateExperience();
-  updateScrollIndicator();
 });
 
 document.getElementById("prev-experience").addEventListener("click", () => {
   currentIndex = (currentIndex - 1 + totalExperiences) % totalExperiences;
   updateExperience();
-  updateScrollIndicator();
 });
 
 function updateExperience() {
@@ -152,12 +153,6 @@ function updateExperience() {
   experienceInner.style.transform = `translateX(${offset}%)`;
 }
 
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    document.getElementById("loading-screen").style.display = "none";
-    typeWriter("Andrea Megan Sustic", 0); // Start the typewriter after loading screen disappears
-  }, 1500); // Adjust delay as needed
-});
-
-hoverSkillBar();
 drawProfilePic();
+typeWriter("Andrea Megan Sustic", 0);
+hoverSkillBar();
