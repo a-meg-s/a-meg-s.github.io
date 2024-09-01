d7e62eca66d503310e34d92aec8e7167272e799b
Megan <codebymegan@gmail.com>
2024-09-01 21:16:12 +0200

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,143 +1,144 @@
+
 function typeWriter(text, n) {
-  const element = document.getElementById("typewriter");
-  if (n < text.length) {
-    element.innerHTML =
-      text.substring(0, n + 1) + '<span class="blinking-cursor">|</span>';
-    setTimeout(() => typeWriter(text, n + 1), 100);
-  } else {
-    element.innerHTML = text + '<span class="blinking-cursor">|</span>';
-    // Ensure the cursor keeps blinking after typing is finished
-    setInterval(() => {
-      const cursor = document.querySelector(".blinking-cursor");
-      if (cursor) {
-        cursor.style.visibility =
-          cursor.style.visibility === "hidden" ? "visible" : "hidden";
-      }
-    }, 500);
-  }
+    const element = document.getElementById("typewriter");
+    if (n < text.length) {
+        element.innerHTML =
+            text.substring(0, n + 1) + '<span class="blinking-cursor">|</span>';
+        setTimeout(() => typeWriter(text, n + 1), 100);
+    } else {
+        element.innerHTML = text + '<span class="blinking-cursor">|</span>';
+        // Ensure the cursor keeps blinking after typing is finished
+        setInterval(() => {
+            const cursor = document.querySelector(".blinking-cursor");
+            if (cursor) {
+                cursor.style.visibility =
+                    cursor.style.visibility === "hidden" ? "visible" : "hidden";
+            }
+        }, 500);
+    }
 }
 
 function drawProfilePic() {
-  const canvas = document.getElementById("profile-pic");
-  if (canvas.getContext) {
-    const ctx = canvas.getContext("2d");
+    const canvas = document.getElementById("profile-pic");
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
 
-    ctx.fillStyle = "rgb(245,207,186)";
-    ctx.fillRect(54, 45, 91, 90);
+        ctx.fillStyle = "rgb(245,207,186)";
+        ctx.fillRect(54, 45, 91, 90);
 
-    // Mouth
-    ctx.fillStyle = "rgb(240,168,137)";
-    ctx.fillRect(93, 117, 15, 5);
-    ctx.fillRect(88, 112, 5, 5);
-    ctx.fillRect(108, 112, 5, 5);
+        // Mouth
+        ctx.fillStyle = "rgb(240,168,137)";
+        ctx.fillRect(93, 117, 15, 5);
+        ctx.fillRect(88, 112, 5, 5);
+        ctx.fillRect(108, 112, 5, 5);
 
-    // Nose
-    ctx.fillStyle = "rgb(240,168,137)";
-    ctx.fillRect(97, 95, 6, 10);
+        // Nose
+        ctx.fillStyle = "rgb(240,168,137)";
+        ctx.fillRect(97, 95, 6, 10);
 
-    // Blush
-    ctx.fillStyle = "rgb(252,144,157)";
-    ctx.fillRect(65, 97, 15, 8);
-    ctx.fillRect(120, 97, 15, 8);
+        // Blush
+        ctx.fillStyle = "rgb(252,144,157)";
+        ctx.fillRect(65, 97, 15, 8);
+        ctx.fillRect(120, 97, 15, 8);
 
-    // Hair
-    ctx.fillStyle = "rgb(150,99,57)";
-    ctx.fillRect(70, 30, 60, 15);
-    ctx.fillRect(50, 40, 30, 20);
-    ctx.fillRect(120, 40, 30, 20);
-    ctx.fillRect(42, 55, 15, 110);
-    ctx.fillRect(143, 55, 15, 110);
-    ctx.fillRect(50, 130, 15, 10);
-    ctx.fillRect(135, 130, 15, 10);
-    ctx.fillRect(55, 135, 15, 35);
-    ctx.fillRect(130, 135, 15, 35);
+        // Hair
+        ctx.fillStyle = "rgb(150,99,57)";
+        ctx.fillRect(70, 30, 60, 15);
+        ctx.fillRect(50, 40, 30, 20);
+        ctx.fillRect(120, 40, 30, 20);
+        ctx.fillRect(42, 55, 15, 110);
+        ctx.fillRect(143, 55, 15, 110);
+        ctx.fillRect(50, 130, 15, 10);
+        ctx.fillRect(135, 130, 15, 10);
+        ctx.fillRect(55, 135, 15, 35);
+        ctx.fillRect(130, 135, 15, 35);
 
-    ctx.fillStyle = "rgb(43,112,52)";
-    ctx.fillRect(70, 135, 60, 65);
+        ctx.fillStyle = "rgb(43,112,52)";
+        ctx.fillRect(70, 135, 60, 65);
 
-    drawEyes(ctx);
-  }
+        drawEyes(ctx);
+    }
 }
 
 function drawEyes(ctx) {
-  ctx.fillStyle = "rgb(69,45,23)";
-  ctx.fillRect(72, 85, 12, 12);
-  ctx.fillRect(117, 85, 12, 12);
+    ctx.fillStyle = "rgb(69,45,23)";
+    ctx.fillRect(72, 85, 12, 12);
+    ctx.fillRect(117, 85, 12, 12);
 }
 
 function blinkEyes(ctx) {
-  ctx.fillStyle = "rgb(245,207,186)";
-  ctx.fillRect(72, 85, 12, 12);
-  ctx.fillRect(117, 85, 12, 12);
+    ctx.fillStyle = "rgb(245,207,186)";
+    ctx.fillRect(72, 85, 12, 12);
+    ctx.fillRect(117, 85, 12, 12);
 
-  ctx.fillStyle = "rgb(69,45,23)";
-  ctx.fillRect(72, 93, 12, 4);
-  ctx.fillRect(117, 93, 12, 4);
+    ctx.fillStyle = "rgb(69,45,23)";
+    ctx.fillRect(72, 93, 12, 4);
+    ctx.fillRect(117, 93, 12, 4);
 
-  setTimeout(() => drawEyes(ctx), 200);
+    setTimeout(() => drawEyes(ctx), 200);
 }
 
 function animateSkillBar(bar, percentage) {
-  let width = 1;
-  const id = setInterval(() => {
-    if (width >= percentage) {
-      clearInterval(id);
-    } else {
-      width++;
-      bar.style.width = width + "%";
-    }
-  }, 10);
+    let width = 1;
+    const id = setInterval(() => {
+        if (width >= percentage) {
+            clearInterval(id);
+        } else {
+            width++;
+            bar.style.width = width + "%";
+        }
+    }, 10);
 }
 
 function reverseSkillBar(bar) {
-  let width = parseInt(bar.style.width, 50);
-  const id = setInterval(() => {
-    if (width <= 25) {
-      clearInterval(id);
-      bar.style.width = "25%";
-    } else {
-      width--;
-      bar.style.width = width + "%";
-    }
-  }, 5000);
+    let width = parseInt(bar.style.width, 50);
+    const id = setInterval(() => {
+        if (width <= 25) {
+            clearInterval(id);
+            bar.style.width = "25%";
+        } else {
+            width--;
+            bar.style.width = width + "%";
+        }
+    }, 5000);
 }
 
 function hoverSkillBar() {
-  const skillBars = document.querySelectorAll("#language");
+    const skillBars = document.querySelectorAll("#language");
 
-  skillBars.forEach((bar) => {
-    bar.addEventListener("mouseenter", () => {
-      if (!bar.classList.contains("filled")) {
-        const percentage = bar.getAttribute("data-percentage");
-        animateSkillBar(bar, percentage);
-        bar.classList.add("filled");
-      }
-    });
+    skillBars.forEach((bar) => {
+        bar.addEventListener("mouseenter", () => {
+            if (!bar.classList.contains("filled")) {
+                const percentage = bar.getAttribute("data-percentage");
+                animateSkillBar(bar, percentage);
+                bar.classList.add("filled");
+            }
+        });
 
-    bar.addEventListener("mouseleave", () => {
-      setTimeout(() => {
-        reverseSkillBar(bar);
-        bar.classList.remove("filled");
-      }, 1000);
+        bar.addEventListener("mouseleave", () => {
+            setTimeout(() => {
+                reverseSkillBar(bar);
+                bar.classList.remove("filled");
+            }, 1000);
+        });
     });
-  });
 }
 
 window.addEventListener("click", () => {
-  const canvas = document.getElementById("profile-pic");
-  if (canvas && canvas.getContext) {
-    const ctx = canvas.getContext("2d");
-    blinkEyes(ctx);
-  }
+    const canvas = document.getElementById("profile-pic");
+    if (canvas && canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+        blinkEyes(ctx);
+    }
 });
 
 // Experience Carousel Logic
 
 function updateScrollIndicator() {
-  const indicatorDots = document.querySelectorAll(".indicator-dot");
-  indicatorDots.forEach((dot, index) => {
-    dot.classList.toggle("active", index === currentIndex);
-  });
+    const indicatorDots = document.querySelectorAll(".indicator-dot");
+    indicatorDots.forEach((dot, index) => {
+        dot.classList.toggle("active", index === currentIndex);
+    });
 }
 
 let currentIndex = 0;
@@ -147,22 +148,65 @@ const experiences = document.querySelectorAll(".experience-inner article");
 const totalExperiences = experiences.length;
 
 document.getElementById("next-experience").addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % totalExperiences;
-  updateExperience();
-  updateScrollIndicator();
+    currentIndex = (currentIndex + 1) % totalExperiences;
+    updateExperience();
+    updateScrollIndicator();
 });
 
 document.getElementById("prev-experience").addEventListener("click", () => {
-  currentIndex = (currentIndex - 1 + totalExperiences) % totalExperiences;
-  updateExperience();
-  updateScrollIndicator();
+    currentIndex = (currentIndex - 1 + totalExperiences) % totalExperiences;
+    updateExperience();
+    updateScrollIndicator();
 });
 
 function updateExperience() {
-  const offset = -currentIndex * 100;
-  experienceInner.style.transform = `translateX(${offset}%)`;
+    const offset = -currentIndex * 100;
+    experienceInner.style.transform = `translateX(${offset}%)`;
 }
 
+// Matrix animation script
+
+(function() {
+    var canvas = document.getElementById("matrix-canvas");
+    var ctx = canvas.getContext("2d");
+
+    // Resize canvas to full screen
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+
+    resizeCanvas();
+    window.addEventListener('resize', resizeCanvas);
+
+    var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}".split("");
+    var font_size = 10;
+    var columns = canvas.width / font_size;
+    var drops = Array(columns).fill(1);
+
+    function drawMatrix() {
+        ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = "#4fff4f";
+        ctx.font = font_size + "px arial";
+
+        drops.forEach((y, i) => {
+            var text = matrix[Math.floor(Math.random() * matrix.length)];
+            ctx.fillText(text, i * font_size, y * font_size);
+
+            if (y * font_size > canvas.height && Math.random() > 0.975) {
+                drops[i] = 0;
+            }
+
+            drops[i]++;
+        });
+    }
+
+    setInterval(drawMatrix, 35);
+})();
+
+// Initialize everything
 drawProfilePic();
 typeWriter("Andrea Megan Sustic", 0);
 hoverSkillBar();
