6db6ad58a67d5de46d8820dac596bc557ef2b607
Megan <codebymegan@gmail.com>
2024-09-01 21:57:45 +0200

    Update resume.js

--- resume.js
+++ resume.js
@@ -131,7 +131,7 @@ window.addEventListener("click", () => {
   }
 });
 
-// Experience Carousel Logic
+// Experience Carousel Logic 
 
 function updateScrollIndicator() {
   const indicatorDots = document.querySelectorAll(".indicator-dot");
